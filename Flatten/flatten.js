061d3300feaa4e721230cf11db0301354e6d27bd
JithuVS <sreejithsalgunan@gmail.com>
2022-04-14 14:07:48 +0530

    updated faltten

--- Flatten/flatten.js
+++ Flatten/flatten.js
@@ -77,3 +77,21 @@ arr = [1, 2, [3, 4, [5, 6]]];
 const flattened = [...g_flatten(arr, Infinity)];
 // [1, 2, 3, 4, 5, 6]
 console.log('generators', flattened);
+
+
+//using array.flat
+const arr1 = [1, 2, [3, 4]];
+arr1.flat(); 
+// [1, 2, 3, 4]
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+arr2.flat();
+// [1, 2, 3, 4, [5, 6]]
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+arr3.flat(2);
+// [1, 2, 3, 4, 5, 6]
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+arr4.flat(Infinity);
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+/* Flatten Array Holes */
+const arr5 = [1, 2, , 4, 5];
+arr5.flat();
