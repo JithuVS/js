610aa2aac7befbe42c12cbff1359cc5af4734bf7
JithuVS <sreejithsalgunan@gmail.com>
2022-04-26 06:12:54 +0530

    append and delete

---
+++ HackerRank/append&delete.js
@@ -0,0 +1,12 @@
+function appendAndDelete(s, t, k) {
+    let o = s.length + t.length;
+    if (k > o) return 'Yes';
+    for (let i = 0, l = Math.min(s.length, t.length); i < l; i++, o -= 2) {
+        if (s[i] !== t[i]) break
+    }
+    return o > k || (k - o) % 2 !== 0 ? 'No' : 'Yes'
+}
+
+
+let result = appendAndDelete('hackerEarth', 'hackerRank', 9);
+console.log(result); 
